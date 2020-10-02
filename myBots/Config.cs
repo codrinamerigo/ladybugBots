92b92b6afb81042799cd6d4a97ad3703e9093fab
Codrina Merigo <codrinamerigo@outlook.com>
2020-10-02 17:29:58 +0200

    cleaned keys

--- myBots/Config.cs
+++ myBots/Config.cs
@@ -8,12 +8,12 @@ namespace myBots
     {
         public static string WebChatBot
         {
-            get => "rP7HEHxWw3w.JDqm4TpeUqiqKEyIgzKEKpu2ZwTB6ctq9zNSG-ZCZuw";
+            get => ""; //azure key 
 
         }
         public static string DirectLineBot
         {
-            get => "ymu0OjYMeOw.sVP6AIHRpsk-RKu1fwOCUhWlR_M9CwE8KW8-x07sJIk";
+            get => ""; // azure key 
 
         }
 
